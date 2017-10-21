15f0eff28b37db6e4c025a15296bc12df0509b1f
Nick Whitelegg <nick@www.free-map.org.uk>
2017-10-21 16:01:19 +0100

    minor layout alterations

--- html/index.php
+++ html/index.php
@@ -9,9 +9,24 @@
 </head>
 
 <body onload='init()'>
-<p>
+<div id="logodiv">
 <img src="images/osmuk.png" alt="OSM UK" width='100px' />
-</p>
+</div>
+<div id="whatis"> 
+Development site for the OSM-UK map. Please note, only Hampshire and West
+Sussex are covered. <a href='https://github.com/osmuk/website_real'>
+GitHub repo</a>
+</div>
+<div id="loginform">
+<form method="post" action="">
+<label for="username">OSM Username:</label>
+<input name="username" id="username" />
+<label for="password">OSM Password:</label>
+<input name="password" id="password" type="password" />
+<input type="submit" value="Go!" />
+</form>
+</div>
+<div id="container">
 <?php
 
 // TODO: oauth authentication with OSM 
@@ -22,17 +37,8 @@ if(isset($_POST["username"])) {
 } else { 
 ?>
 
-<div id="container">
 
-<div id="loginform">
-<form method="post" action="">
-<label for="username">OSM Username:</label>
-<input name="username" id="username" />
-<label for="password">OSM Password:</label>
-<input name="password" id="password" type="password" />
-<input type="submit" value="Go!" />
-</form>
-</div>
+
 
 <div id="map"></div>
 
